1aee95926e27b10982031917c1243e4dfed97a07
Saurabh <saurabhs199927@gmail.com>
2022-12-15 15:24:32 +0530

    lombok dependency is added

---
+++ src/main/java/in/ashokit/security/model/Book.java
@@ -0,0 +1,14 @@
+package in.ashokit.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+	
+	private String name;
+	private int id;
+	private long phone;
+
+}
---
+++ src/main/java/in/ashokit/security/model/User.java
@@ -0,0 +1,9 @@
+package in.ashokit.security.model;
+
+
+public class User {
+	
+	private String name;
+	private int id;
+	private double price;
+}
